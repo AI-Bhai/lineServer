0c01ec3d6e11e4f418f396fb191239d7404ce6f9
Jenwit <jenwitkmonz@gmail.com>
2018-07-30 12:34:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AI System
+ * Date: 20-Jul-18
+ * Time: 12:43 PM
+ */
+
+require_once 'vendor/autoload.php';
+require_once 'function.php';
+require_once 'class/class.php';
+require_once 'includes/includes.php';
+
+echo 'Line bot sever is running'; 
